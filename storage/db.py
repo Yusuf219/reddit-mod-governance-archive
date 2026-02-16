fa8cab5d18488347d33ff736e2ab48abe5f3383b
Yusuf219 <115668129+Yusuf219@users.noreply.github.com>
2026-02-16 10:31:45 +0000

    Create db.py
    Initial database placeholder

---
+++ storage/db.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./rmga.db") # Noted for when I go live with this
+
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
